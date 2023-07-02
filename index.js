587a3a4e4997ee5a2f66ec746a43504dec23593d
ipglass <irinadze@gmail.com>
2023-07-02 12:28:04 +0300

    Added some code

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const app = express();
+var cors = require("cors");
+const bodyParser = require("body-parser");
+
+//const questionRoutes = require("./api/routes/question");
+//const answerRoutes = require("./api/routes/answer");
+//const userRoutes = require("./api/routes/user"); 
+
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
+
+mongoose
+  .connect(process.env.MONGO_CONNECTION, { useNewUrlParser: true })
+  .then(console.log("connected"))
+  .catch((err) => {
+    console.log("DB connection error:", err);
+    console.log(err);
+  });
+
+//app.use(questionRoutes);
+//app.use(answerRoutes);
+//app.use(userRoutes);
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, PUT, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
+app.listen(8080);
+
+
+
+
